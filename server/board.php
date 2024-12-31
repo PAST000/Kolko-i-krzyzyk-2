a5e5070c805616764e3345d19ede96d2804d845b
PAST000 <adam_st2007@wp.pl>
2024-12-31 17:18:29 +0100

    Multidimensional board2

--- server/board.php
+++ server/board.php
@@ -1,32 +1,32 @@
 <?php 
 class Board{
-    private $board = [];
-    private $dims = 3;  //Liczba wymiarów
-    private $sizeX;
-    private $sizeY;
-    private $sizeZ;
-    private $sizes = [];
-
-    private $target; //Zwycięzka ilość w jednej linii
+    private $board = [];    // Tablica z postawionymi pionkami, puste pole - null
+    private $dims = 3;      // Liczba wymiarów
+    private $sizes = [];    // Tablica z rozmiarami
+    private $sizesMult = 1; // Przemnożone rozmiary
+    private $target = 3;    // Zwycięzka ilość w jednej linii
     private $dirs = [];
-    private const $pawns = ['O', 'K', 'P']; //Kula, krzyżyk, piramida
+    private const $pawns = ['O', 'X', 'P']; // Kula, krzyżyk, piramida
 
     function __construct($sizesArr, $trg){
+        if(count($sizesArr) < 2)
+            throw new Exception("Liczba wymiarów musi być większa od 1");
+        if($trg < 3)
+            throw new Exception("Zwycięzka ilosć w jednej linii musi być większa od 2");
+        for($i = 0; $i < count($sizesArr); $i++){
+            if($sizesArr[$i] <= 0)
+                throw new Exception("Każdy rozmiar musi być dodatni");
+        }
+
         $this->dims = count($sizesArr);
         $this->sizes = $sizesArr;
-        $this->sizeX = $sizesArr[0];
-        $this->sizeY = $sizesArr[1];
-        $this->sizeZ = $sizesArr[2];
         $this->targer = $trg;
-
-        for ($i = 0; $i < $this->sizes[0]; $i++) {
-            $array[$i] = []; 
-            for ($j = 0; $j < $this->sizes[1]; $j++) {
-                $array[$i][$j] = []; 
-                for ($k = 0; $k < $this->sizes[2]; $k++) {
-                    $array[$i][$j][$k] = null; 
-                }
-            }
+        
+        for($i = 0; $i < $this->dims; $i++){
+            $this->sizesMult *= $this->sizes[$i];
+        }
+        for ($i = 0; $i < $this->sizesMult; $i++) {
+            $this->board[$i] = null;
         }
         initDirs();
     }
@@ -39,70 +39,88 @@ class Board{
         }
     }
 
-    function put($x, $y, $z, $id){  //$id - numer "pionka"
-        if($x >= $this->sizes[0] || $y >= $this->sizes[1] || $z >= $this->sizes[2]){
-            return false;
-        }
-        if($this->board[$x][$y][$z] !== null){
-            return false;
-        }
-        if($id >= count($this->pawns) || $id < 0){
-            return false;
-        }
+    function put($id, $pawn){  
+        if($id < 0 || $id >= count($this->board)) return false;
+        if($pawn < 0 || $pawn > count($this->pawns)) return false;
 
-        $this->board[$x][$y][$z] = $this->pawns[$id];
+        $this->board[$id] = $this->pawns[$pawn];
         return true;
     }
 
-    function clear($x, $y, $z){
-        if($x >= $this->sizes[0] || $y >= $this->sizes[1] || $z >= $this->sizes[2]){
-            return false;
-        }
-        if($this->board[$x][$y][$z] === null){
-            return false;
-        }
-        
-        $this->board[$x][$y][$z] = null;
+    function clear($id){
+        if($id < 0 || $id >= count($this->board)) return false;
+        $this->board[$id] = null;
         return true;
     }
 
-    function clearPlayer($id){}
+    function clearPlayer($id){
+        if($id < 0 || $id >= count($this->board)) return false;
+        for($i = 0; $i < count($this->board); $i++){
+            if($this->board[$i] == $this->pawns[$id]){
+                $this->board[$i] = null;
+            }
+        }
+        return true;
+    }
 
-    function check(){
-        for($i = 0; $i < $this->sizes[0]; $i++){
-            for($j = 0; $j < $this->sizes[1]; $j++){
-                for($k = 0; $k < $this->sizes[2]; $k++){
-                    if(board[$i][$j][$k] === null) continue;
+    function check(){  //Zwraca id pola i id kierunku
+        for($i = 0; $i < count($this->board); $i++){
+            if($this->board[$i] === null) continue;
 
-                    for($n = 0; $n < count($this->dirs); $n++){
-                        if(!isInRange($i, $j, $k, $n)) continue;
+            for($j = 0; $j < count($this->dirs); $j++){
+                if(!isInRange($i, $j)) continue;
 
-                        for($m = 0; $m < $this->target; $m++){
-                            if($this->board[$i + $m * $this->dirs[$n][0]][$j + $m * $this->dirs[$n][1]][$k + $m * $this->dirs[$n][2]] != $this->board[$i][$j][$k]) break;
-                            if($m == $this->target - 1) return $this->board[$i][$j][$k];
-                        }
-                    }
+                for($k = 0; $k < $this->target; $k++){
+                    if($this->board[$i] !== $this->board[arrToId(translate($i, $this->dirs[$j], $k))]) break;
+                    if($k === $this->target - 1) return [$i, $j];  //Jeśli sprawdziliśmy już wystarczającą do wygranej ilość pól
                 }
             }
         }
         return null;
     }
 
-    function isInRange($x, $y, $z, $n){ //n - indeks kierunków
-        $x += $this->target*($this->dirs[$n][0]); 
-        $y += $this->target*($this->dirs[$n][1]);
-        $z += $this->target*($this->dirs[$n][2]);
-
-        if($x < 0 || $x >= $this->sizes[0]){
-            return false;
-        }
-        if($y < 0 || $y >= $this->sizes[1]){
-            return false;
-        }
-        if($z < 0 || $z >= $this->sizes[2]){
-            return false;
+    function isInRange($id, $n){ //n - indeks kierunków
+        if($id < 0 || $id >= count($this->board)) return false;
+        $v = idToArr($id);
+        for($i = 0; $i < $this->dims: i++){
+            $v[$i] += $this->target*($this->dirs[$n][$i]);
+            if($v[$i] < 0 || $v[$i] >= $this->sizes[$i])
+                return false;
         }
         return true;
+    }
+
+    function translate($id, $v, $k = 1){
+        if($id < 0 || $id >= count($this->board)) return false;
+        if(count($v) < $this->dims) return false;
+
+        $translated = $this->board[$id];
+        for($i = 0; $i < count($translated); $i++){
+            $translated[$i] *= ($v[$i] * $k);
+        }
+        return $translated;
+    }
+
+    function idToArr($id){
+        if($id < 0 || $id >= count($this->board)) return false;
+        $arr = [];
+        $m = $this->sizesMult;
+        for($i = $this->dims - 1; $i >= 0; $i--){
+            $m /= $this->sizes[$i];
+            $arr[$i] = floor($id / $m);
+            $id -= arr[$i] * $m;
+        }
+    }
+
+    function arrToId($v){
+        if(count($v) < $this->dims) return false;
+        $id = 0;
+        $m = 1;
+        for($i = 0; $i < $this->dims; $i++){
+            $id += $v[$i] * $m;
+            $m *= $this->sizes[$i];
+        }
+        return $id;
     }
 
     function getTarget(){
