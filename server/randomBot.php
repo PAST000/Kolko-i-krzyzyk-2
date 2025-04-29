5ba48f1ca4dfc87d123c0371927e229c513a3d22
PAST000 <adam_st2007@wp.pl>
2025-04-29 21:48:42 +0200

    randomBot

---
+++ server/randomBot.php
@@ -0,0 +1,39 @@
+<?php
+class RandomBot{
+    private $boardSize = 0;
+    private $boardSeparator = ',';
+    public const PAWNS = ['O', 'X', 'P'];
+
+    public function __construct($boardSize){
+        $this->boardSize = $boardSize;
+    }
+
+    public function makeMove($txt, $self){
+        $selfPawn = $self;
+        if(is_numeric($self)){
+            if($self < 0 || $self >= count(self::PAWNS) || !is_int($self)) return false;
+            $selfPawn = self::PAWNS[$self];
+        }
+        else
+            if(array_search($self, self::PAWNS) === false) return false;
+
+        $board = explode($this->boardSeparator, $txt);
+        if(count($board) !== $this->boardSize) return false;
+
+        $availableFields = [];
+        for($i = 0; $i < count($board); $i++)
+            if(empty($board[$i])) 
+                $availableFields[] = $i;
+
+        if(count($availableFields) === 0) return false;
+        return $availableFields[rand(0, count($availableFields) - 1)];
+    }
+
+    public function setBoardSeparator($separator){ 
+        if(strlen($separator) !== 1) return false;
+        $this->boardSeparator = $separator; 
+        return true;
+    }
+    public function getBoardSeparator(){ return $this->boardSeparator; }
+}
+?>
