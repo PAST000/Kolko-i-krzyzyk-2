634016e7288320d0e59d5f44f7817d7c98001705
PAST000 <adam_st2007@wp.pl>
2025-05-17 22:56:29 +0200

    Admin modal

--- game/script.js
+++ game/script.js
@@ -10,6 +10,14 @@ function changeLineColor(){
     preview.style.opacity = (document.getElementById("lineAlpha").value/100);
 }
 
+function showError(text){
+    if(!text) text = "Błąd w pobieraniu danych.";
+    sessionStorage.removeItem("errorData");
+    sessionStorage.setItem("errorData", JSON.stringify({ txt : text }));
+    document.getElementById("errorWrapper").style.display = "block";
+    document.getElementById("errorIframe").contentWindow.postMessage("showError", "*");
+}
+
 function refreshInfo(players, target, turn, yourID, admin){
     document.getElementById("turn").innerHTML = "Tura gracza: " + turn;
     document.getElementById("yourID").innerHTML = "Twoje ID: " + yourID;
@@ -25,12 +33,6 @@ function refreshInfo(players, target, turn, yourID, admin){
         wrapper.classList.add("playerWrapper");
         par.appendChild(txt);
         wrapper.appendChild(par);
-
-        if(admin){
-            const button = document.createElement("button");
-            button.classList.add("kickButton");
-            wrapper.appendChild(button);
-        }
         childrenArray.push(wrapper);
     }
     document.getElementById("playersContainer").replaceChildren(...childrenArray);
@@ -40,10 +42,12 @@ window.addEventListener("message", function(event) {
     if(event.data === "closeModal"){
         document.getElementById("howToWrapper").style.display = "none"; 
         document.getElementById("controlWrapper").style.display = "none"; 
-        document.getElementById("winWrapper").style.display = "none";   
+        document.getElementById("winWrapper").style.display = "none";
+        document.getElementById("errorWrapper").style.display = "none";
+        document.getElementById("adminWrapper").style.display = "none";     
     }
-    else if(event.data === "closeInfo"){
-        document.getElementById("infoWrapper").style.display = "none";  
+    else if(event.data === "closeError"){
+        document.getElementById("errorWrapper").style.display = "none";  
         window.location.href = "../";
     }
 });
@@ -55,11 +59,25 @@ window.onload = function() {
     document.getElementById("howToWrapper").style.display = "none"; 
     document.getElementById("controlWrapper").style.display = "none"; 
     document.getElementById("winWrapper").style.display = "none";   
-    document.getElementById("infoWrapper").style.display = "none"; 
+    document.getElementById("errorWrapper").style.display = "none"; 
+    document.getElementById("adminWrapper").style.display = "none"; 
 
     document.getElementById("gameTitle").onclick = function(){ document.location.href = "../"; };
     document.getElementById("howToButton").onclick = function(){ document.getElementById("howToWrapper").style.display = "block"; };
     document.getElementById("controlButton").onclick = function(){ document.getElementById("controlWrapper").style.display = "block"; };
     document.getElementById("resultsButton").onclick = function(){ document.location.href = "../results"; }
     document.getElementById("error").ondblclick = function(){ document.getElementById("error").innerHTML = ""; }
+
+    document.getElementById("adminButton").onclick = function(){ 
+        let nodeList = document.getElementById("playersContainer").childNodes; 
+        let data = [];
+        for(let i = 0; i < nodeList.length; i++){
+            let arr = nodeList[i].childNodes[0].innerText.split(' ');
+            data.push(new Array(arr[0].substr(1).substr(arr[0].length - 3, 1), arr[1]));
+        }
+        sessionStorage.removeItem("adminData");
+        sessionStorage.setItem("adminData", JSON.stringify({ players: data }));
+        document.getElementById("adminWrapper").style.display = "block";
+        document.getElementById("adminIframe").contentWindow.postMessage("showAdmin", "*"); 
+    };
 }
